e4494f7aec5ada55309bf2636c0bdc619310fc97
joypaul <jpaul3250@gmail.com>
2019-07-31 18:21:12 -0400

    data bindings of strings using a seperate class

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -6,19 +6,19 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     //lateinit binding var
     private lateinit var binding: ActivityMainBinding
+
+    //create a var to hold data from the data class
+    private var myName: MyNameData = MyNameData(name = "Joy Paul")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 //        findViewById<Button>(R.id.on_edit_done_button).setOnClickListener {
 //            addNickname(it)
 //        }
@@ -26,6 +26,8 @@ class MainActivity : AppCompatActivity() {
         //use binding to access views and data. you first enable it on build.gradle on app folder
         //using binding is more performance focused
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
 
         //using binding + widget id we set on click listener
         binding.onEditDoneButton.setOnClickListener {
@@ -38,8 +40,10 @@ class MainActivity : AppCompatActivity() {
 
         //using binding.apply we set multiple widgets
         binding.apply {
-            nicknameTextView.text = binding.nicknameEdit.text
-            invalidateAll()//force rebind of widgets 
+            //nicknameTextView.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString() //setting myName's nickname to the input text and it
+            //would update it in the widget since it's all binded in the xml
+            invalidateAll()//force rebind of widgets
             nicknameEdit.visibility = View.GONE
             onEditDoneButton.visibility = View.GONE
             nicknameTextView.visibility = View.VISIBLE
---
+++ app/src/main/java/com/example/aboutme/MyNameData.kt
@@ -0,0 +1,4 @@
+package com.example.aboutme
+
+//holds some static data
+data class MyNameData(var name: String = "", var nickname: String = "")
